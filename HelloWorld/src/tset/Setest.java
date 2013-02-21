198ff670b974b161c8852cc019d8b171455fb801
Martin Zhang <martin.zhang@tuohesoft.com>
2013-02-21 13:53:00 +0800

    tewst

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 package tset;
 
 public class Setest {
 
 	public static void main(String[] args) {
-		System.out.println("OkNoK");
-		System.out.println("DONE");
-	}
-
-}
-=======
-package tset;
-
-public class Setest {
-
-	public static void main(String[] args) {
-		System.out.println("OkkkkkkkkDONE");
+		System.out.println("Okkkk00000kkkkDONE");
+		System.out.println("OaaaaaaakkkkkkkkDONE");
 		System.out.println("OKKKKK");
 	}
 
 }
->>>>>>> 8f58a49cc437b294370133d1f6bd6e388c004791
+
