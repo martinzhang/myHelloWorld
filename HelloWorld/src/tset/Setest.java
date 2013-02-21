06111613223006406d3e608cae0c8b2484192c10
Martin Zhang <martin.zhang@tuohesoft.com>
2013-02-21 13:50:44 +0800

    change some things

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -3,7 +3,7 @@ package tset;
 public class Setest {
 
 	public static void main(String[] args) {
-		System.out.println("DONE");
+		System.out.println("OkkkkkkkkDONE");
 		System.out.println("OKKKKK");
 	}
 
