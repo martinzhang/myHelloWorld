24c9bc95f853ab75a7ac91b916f304673dc8b642
Martin Zhang <martin.zhang@tuohesoft.com>
2013-02-21 14:11:36 +0800

    change

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -2,17 +2,11 @@ package tset;
 
 public class Setest {
 
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		
+	public static void main(String[] args) {		
 		System.out.println("Okkkk00000kkkkDONE");
-		
 		System.out.println("OaaaaaaakkkkkkkkDONE");
-		
-=======
 		System.out.println("Okkkk00000kkkkDONEbbbb");
 		System.out.println("OaaaaaaakkkkkkkkDONEaaaaa");
->>>>>>> 81e8794f4f9d7c9615bf508e7cc8144475a8f7e1
 		System.out.println("OKKKKK");
 	}
 
