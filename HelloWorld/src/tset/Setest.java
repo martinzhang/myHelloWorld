81e8794f4f9d7c9615bf508e7cc8144475a8f7e1
Martin Zhang <martin.zhang@tuohesoft.com>
2013-02-21 13:57:52 +0800

    change message

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -3,8 +3,8 @@ package tset;
 public class Setest {
 
 	public static void main(String[] args) {
-		System.out.println("Okkkk00000kkkkDONE");
-		System.out.println("OaaaaaaakkkkkkkkDONE");
+		System.out.println("Okkkk00000kkkkDONEbbbb");
+		System.out.println("OaaaaaaakkkkkkkkDONEaaaaa");
 		System.out.println("OKKKKK");
 	}
 
