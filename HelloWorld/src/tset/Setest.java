feae96a00675c6cd5ac55e0ed3f8508cd9097c34
MZhang <MZhang@mt.t>
2013-02-22 11:47:48 +0800

    add new comment

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -3,6 +3,7 @@ package tset;
 public class Setest {
 	// brance testing new add
 	// new add 2
+	// this is testing branch added line commment......
 	public static void main(String[] args) {		
 		System.out.println("Okkkk00000kkkkDONE");
 		System.out.println("OaaaaaaakkkkkkkkDONE"); 
