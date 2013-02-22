a247cea267a2367b859b32f5f842647a10da1023
MZhang <MZhang@mt.t>
2013-02-22 10:59:46 +0800

    add comment 2

--- HelloWorld/src/tset/Setest.java
+++ HelloWorld/src/tset/Setest.java
@@ -2,6 +2,7 @@ package tset;
 
 public class Setest {
 	// brance testing new add
+	// new add 2
 	public static void main(String[] args) {		
 		System.out.println("Okkkk00000kkkkDONE");
 		System.out.println("OaaaaaaakkkkkkkkDONE"); 
